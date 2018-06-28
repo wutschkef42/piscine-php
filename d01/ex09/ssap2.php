007d9f62b4306b247b67e132ea9a7e21043cc82e
Felix Wutschke <fwutschk@e1z4r6p23.42.us.org>
2018-06-27 23:29:11 -0700

    ex09 still broken

--- d01/ex09/ssap2.php
+++ d01/ex09/ssap2.php
@@ -1,14 +1,37 @@
 #!/usr/bin/env php
 <?php
-
 $words = array();
 foreach (array_slice($argv, 1) as $val)
 {
-	print_r($val);
-	//$words = array_merge($words, preg_split('/ +/', $val));
+	$words = array_merge($words, preg_split('/ +/', $val));
 }
-//natcasesort($words);
-/*foreach ($words as $word)
+
+$alpha = array();
+$numeric = array();
+
+foreach ($words as $word)
 {
-	echo $word, "\n";
-}*/
+	if (ctype_alpha($word[0]))
+	{
+		array_push($alpha, $word);
+		$words = array_diff($words, array($word));
+	}
+	if (is_numeric($word))
+	{
+		array_push($numeric, $word);
+		$words = array_diff($words, array($word));	
+	}
+}
+
+var_dump($words);
+var_dump($alpha);
+var_dump($numeric);
+
+usort($alpha, "strcmp");
+rsort($numeric, SORT_NUMERIC);
+sort($words);
+
+$alpha = array_merge($alpha, $numeric);
+$alpha = array_merge($alpha, $words);
+
+var_dump($alpha);
